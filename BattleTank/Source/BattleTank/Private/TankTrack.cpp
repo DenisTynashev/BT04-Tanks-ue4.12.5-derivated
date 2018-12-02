0d7f010663a885e39d6db9cfefad733292ceebf0
Denis-PC\Denis <denis.tynashev@yandex.com>
2018-12-02 21:52:52 +0100

    Лекция 190. Переделано управление тракамию Движущая сила не прилагается, если танк не на земле.

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -10,7 +10,7 @@ UTankTrack::UTankTrack()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	bWantsBeginPlay = true;
-	PrimaryComponentTick.bCanEverTick = true;
+	//PrimaryComponentTick.bCanEverTick = true;
 }
 
 void UTankTrack::BeginPlay()
@@ -21,29 +21,42 @@ void UTankTrack::BeginPlay()
 
 void UTankTrack::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit)
 {
-	UE_LOG(LogTemp, Warning, TEXT("Track on the ground"));
+	DriveTrack();
+	ApplySidewaysForce();
+	CurrentThrottle = 0;
 }
 
 
-void  UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+/*void  UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	
+}*/
+
+void UTankTrack::ApplySidewaysForce()
+{
 	// Calculate the sleepage speed
 	FVector ForwardVector = GetComponentVelocity();
-	FVector RightVector = GetOwner()->GetActorRightVector();	
+	FVector RightVector = GetOwner()->GetActorRightVector();
 	float SleepageSpeed = FVector::DotProduct(ForwardVector, RightVector);
 	//float SleepageSpeed = GetComponentVelocity().Size()*cos;
+	auto DeltaTime = GetWorld()->GetDeltaSeconds();
 	auto CorrectionAcceleration = (-SleepageSpeed / DeltaTime)*GetRightVector();
 
 	auto TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
-	auto CorrectionForce = (TankRoot->GetMass()*CorrectionAcceleration)/2; // two tracks
+	auto CorrectionForce = (TankRoot->GetMass()*CorrectionAcceleration) / 2; // two tracks
 	TankRoot->AddForce(CorrectionForce);
 }
 
 void UTankTrack::SetThrottle(float Throttle)
 {
-	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+	CurrentThrottle = FMath::Clamp<float>(CurrentThrottle + Throttle, -1, 1);
+}
+
+void UTankTrack::DriveTrack()
+{
+	auto ForceApplied = GetForwardVector() * CurrentThrottle * TrackMaxDrivingForce;
 	auto ForceLocation = GetComponentLocation();
 	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
-	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);	
+	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
 }
--- BattleTank/Source/BattleTank/Public/TankTrack.h
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -20,17 +20,24 @@ public:
 	
 	// Max force per track, in Newtons
 	UPROPERTY(EditDefaultsOnly)
-	float TrackMaxDrivingForce = 400000; // Assume 40 tonne tank, and 1g accelleration
+	float TrackMaxDrivingForce = 100000; // Assume 40 tonne tank, and 1g accelleration
 
 private:
 	//Constructor
 	UTankTrack();
 
 	//Tick	
-	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
+	//virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
+
+	void ApplySidewaysForce();
 
 	//Begin Play
 	virtual void BeginPlay() override;
+
+	//
+	void DriveTrack();
+
+	float CurrentThrottle = 0.f;
 
 	UFUNCTION()
 	void OnHit (UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
